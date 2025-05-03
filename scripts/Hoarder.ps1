2aeb6be86612e97f1652aa43da746f36cd332f39
hayet7 <khalfaouihayet30@gmail.com>
2025-05-03 11:29:51 +0200

    Premier commit : ajout des scripts

---
+++ scripts/Hoarder.ps1
@@ -0,0 +1,10 @@
+# Télécharger Hoarder depuis GitHub
+cd $env:USERPROFILE\Downloads
+git clone https://github.com/DFIRKuiper/Hoarder.git
+
+
+# Aller dans le dossier releases
+cd .\Hoarder\releases\
+
+# Exécuter hoarder.exe
+.\hoarder.exe -vv
